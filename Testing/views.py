e71715822bc2c6d01a1b9c0fe4472ecf3df5b26b
Khromov <cckkcckk666888@gmail.com>
2022-11-10 03:11:12 +0700

    max delaet

--- Testing/views.py
+++ Testing/views.py
@@ -230,7 +230,7 @@ class Test(APIView):
     def get(self, request):
         questions, level = self.testing_algorithm.get_start_questions()
         self.level = level
-        print(self.level)
+        print(self.level, 'One')
         serializer = serializers.QuestionsSerializer(questions, many=True)
 
         return Response(
@@ -247,10 +247,10 @@ class Test(APIView):
             )
             next_level = self.testing_algorithm.is_next_level()
 
-        print(self.level)
+        print(self.level, 'Two')
         questions, level = self.testing_algorithm.get_questions(next_level, self.level)
         self.level = level
-        print(self.level)
+        print(self.level, 'Three')
         serializer = serializers.QuestionsSerializer(questions, many=True)
 
         return Response(
